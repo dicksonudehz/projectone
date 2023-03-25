c4a15b65ea80cb3a091d0dec3b34ab6b4227400f
github_dicksonudehz <dicksonudehz.dicksonudehz@gmail.com>
2023-03-25 00:49:53 -0700

    clear unused pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import HomepageFour from "./HomepageFour";
-import Homepageone from "./Homepageone";
-import Homepagethree from "./Homepagethree";
-import HomepageTwo from "./HomepageTwo";
+// import Homepageone from "./Homepageone";
+// import Homepagethree from "./Homepagethree";
+// import HomepageTwo from "./HomepageTwo";
 
 function App() {
   return (
